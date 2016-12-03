3273b2fcf13f0dbb8cfe484b35e5e43f712f25cd
hejtmy <hejtmy@gmail.com>
2016-12-03 22:50:57 +0100

    Adds colour scheme scriptable object

---
+++ Assets/BrainWaves/Code/Settings/_brain_ColourScheme.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _brain_ColourScheme : ScriptableObject {
+    public Color scheme1_primary;
+    public Color scheme1_secondary;
+    public Color scheme2_primary;
+    public Color scheme2_secondary;
+}
