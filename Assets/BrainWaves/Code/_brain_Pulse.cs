95773c65d5ea8ed3980ef7a5c0253809c39fe9bb
hejtmy <hejtmy@gmail.com>
2016-12-03 12:11:54 +0100

    renamed brainWaves to prain pulse

---
+++ Assets/BrainWaves/Code/_brain_Pulse.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class _brain_Pulse : _brain_Core {  
+    public Color Color;
+    public float Speed = 1;
+    public float Strength;
+
+
+
+    public IEnumerator pulse;
+    private Vector3 _direction = new Vector3(1, 0, 0);
+
+    public void StartPulsing(float targetScale, float duration)
+    {
+        StopPulsing();
+        pulse = Pulsing(targetScale, duration);
+        StartCoroutine(pulse);
+    }
+    public void StopPulsing()
+    {
+        if (pulse != null) StopCoroutine(pulse);
+    }
+
+    protected IEnumerator Pulsing(float targetScale, float duration)
+    {
+        var currentScaleVec = gameObject.transform.localScale;
+        var targetScaleVec = currentScaleVec * targetScale;
+        while (true)
+        {
+            gameObject.transform.localScale = Vector3.Lerp(currentScaleVec, targetScaleVec, Mathf.PingPong(Time.time, 1));
+            yield return null;
+        }
+    }
+
+}
