04c48200dd6e9d0e690b6c4dd34bafec3bc0d29b
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-12-04 01:48:04 +0100

    Add intro scene

---
+++ Assets/brv_IntroManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brv_IntroManager : MonoBehaviour {
+
+    public GameObject StartExperience;
+    public GameObject Connect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void ConnectEEG()
+    {
+        //do sth
+        Connect.SetActive(false);
+        StartExperience.SetActive(true);
+    }
+
+    public void StartLevel()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
