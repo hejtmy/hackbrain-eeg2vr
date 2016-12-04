e6ecdf2de1546230fa1ac356542569d4b478c6ba
hejtmy <hejtmy@gmail.com>
2016-12-04 10:13:25 +0100

    Singleton addition

---
+++ Assets/BrainWaves/ImportedCode/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected static T instance;
+
+    /**
+       Returns the instance of this singleton.
+    */
+    public static T Get
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null) Debug.LogError("An instance of " + typeof(T) + 
+                    " is needed in the scene, but there is none.");
+            }
+            return instance;
+        }
+    }
+
+    //ADDED return new or existing instance of this singleton.
+    public static T GetOrCreate (GameObject parent)
+    {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+                if (instance == null)
+                {
+                    instance = parent.AddComponent<T>();
+                }
+            }
+            return instance;
+    }
+
+    public static T GetOrCreate()
+    {
+        if (instance == null)
+        {
+            instance = (T)FindObjectOfType(typeof(T));
+            if (instance == null)
+            {
+                GameObject go = new GameObject(typeof(T).Name);
+                instance = go.AddComponent<T>();
+            }
+        }
+        return instance;
+    }
+}
